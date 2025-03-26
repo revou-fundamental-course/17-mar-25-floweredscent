b84999e1b18158905ea896820e4f51be2fb229c5
Syahla Maharani <maharanisyahla@gmail.com>
2025-03-26 22:46:32 +0700

    adding option

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
+function tampilkanform() {
+    let bentuk = document.getElementById("bentuk").value;
+    let formContainer = document.getElementById("forminput");
+    let formPersegi = document.getElementById("formpersegi");
+    let formPersegiPanjang = document.getElementById("formpersegipanjang");
+
+    if (bentuk === "persegi") {
+        formContainer.style.display = "block";
+        formPersegi.style.display = "block";
+        formPersegiPanjang.style.display = "none";
+    } else if (bentuk === "persegiPanjang") {
+        formContainer.style.display = "block";
+        formPersegi.style.display = "none";
+        formPersegiPanjang.style.display = "block";
+    } else {
+        formContainer.style.display = "none";
+    }
+}
+
 function hitungPersegi() {
     let sisi = document.getElementById("sisi").value;
     if (sisi <= 0 || sisi === "") {
